3334a4ca8622e165a6702b4df9e3df38a308372a
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-10-28 07:07:09 +0000

    [Finish] bookers2-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @booknew = Book.new
     @user=@book.user
+    @book_comment = BookComment.new
   end
 
   def index
@@ -52,6 +53,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
   def correct_user
     @book = Book.find(params[:id])
     @user = @book.user
